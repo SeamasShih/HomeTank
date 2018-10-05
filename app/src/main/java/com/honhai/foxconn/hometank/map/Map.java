0789fd8185b5916352f2f7543fdaaac97ce2e86b
ALiao1432 <apple36578@gmail.com>
2018-10-05 15:24:18 +0800

    fix error

--- app/src/main/java/com/honhai/foxconn/hometank/map/Map.java
+++ app/src/main/java/com/honhai/foxconn/hometank/map/Map.java
@@ -10,19 +10,6 @@ public class Map implements Serializable {
         mapData = new MapData[h][w];
     }
 
-    public static Map createTestMap() {
-        Map map = new Map(200, 200);
-        for (int i = 0; i < map.mapData.length; i++) {
-            for (int j = 0; j < map.mapData[0].length; j++) {
-                if ((i + 1) % 2 == 0 || (j + 1) % 2 == 0)
-                    map.mapData[j][i] = MapData.TEST_ROAD;
-                else
-                    map.mapData[j][i] = MapData.TEST_PILLAR;
-            }
-        }
-        return map;
-    }
-
     public MapData getMapData(int x, int y) {
         return mapData[x][y];
     }
